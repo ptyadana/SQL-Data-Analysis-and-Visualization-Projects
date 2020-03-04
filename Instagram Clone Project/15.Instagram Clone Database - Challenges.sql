b0e9f9bfc96c4df6ebce9969b6b4774c9d5b48d9
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-04 11:01:28 +0800

    add Instagram Clone Project

---
+++ Instagram Clone Project/15.Instagram Clone Database - Challenges.sql
@@ -0,0 +1,188 @@
+/*We want to reward our users who have been around the longest.  
+Find the 5 oldest users.*/
+SELECT * FROM users
+ORDER BY created_at
+LIMIT 5;
+
+
+/*What day of the week do most users register on?
+We need to figure out when to schedule an ad campgain*/
+SELECT date_format(created_at,'%W') AS 'day of the week', COUNT(*) AS 'total registration'
+FROM users
+GROUP BY 1
+ORDER BY 2 DESC;
+
+/*version 2*/
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
+
+
+/*We want to target our inactive users with an email campaign.
+Find the users who have never posted a photo*/
+SELECT username
+FROM users
+LEFT JOIN photos ON users.id = photos.user_id
+WHERE photos.id IS NULL;
+
+
+/*We're running a new contest to see who can get the most likes on a single photo.
+WHO WON??!!*/
+SELECT users.username,photos.id,photos.image_url,COUNT(*) AS Total_Likes
+FROM likes
+JOIN photos ON photos.id = likes.photo_id
+JOIN users ON users.id = likes.user_id
+GROUP BY photos.id
+ORDER BY Total_Likes DESC
+LIMIT 1;
+
+/*version 2*/
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
+
+
+/*Our Investors want to know...
+How many times does the average user post?*/
+/*total number of photos/total number of users*/
+SELECT ROUND((SELECT COUNT(*)FROM photos)/(SELECT COUNT(*) FROM users),2);
+
+
+/*user ranking by postings higher to lower*/
+SELECT users.username,COUNT(photos.image_url)
+FROM users
+JOIN photos ON users.id = photos.user_id
+GROUP BY users.id
+ORDER BY 2 DESC;
+
+
+/*Total Posts by users (longer versionof SELECT COUNT(*)FROM photos) */
+SELECT SUM(user_posts.total_posts_per_user)
+FROM (SELECT users.username,COUNT(photos.image_url) AS total_posts_per_user
+		FROM users
+		JOIN photos ON users.id = photos.user_id
+		GROUP BY users.id) AS user_posts;
+
+
+/*total numbers of users who have posted at least one time */
+SELECT COUNT(DISTINCT(users.id)) AS total_number_of_users_with_posts
+FROM users
+JOIN photos ON users.id = photos.user_id;
+
+
+/*A brand wants to know which hashtags to use in a post
+What are the top 5 most commonly used hashtags?*/
+SELECT tag_name, COUNT(tag_name) AS total
+FROM tags
+JOIN photo_tags ON tags.id = photo_tags.tag_id
+GROUP BY tags.id
+ORDER BY total DESC;
+
+
+/*We have a small problem with bots on our site...
+Find users who have liked every single photo on the site*/
+SELECT users.id,username, COUNT(users.id) As total_likes_by_user
+FROM users
+JOIN likes ON users.id = likes.user_id
+GROUP BY users.id
+HAVING total_likes_by_user = (SELECT COUNT(*) FROM photos);
+
+
+/*We also have a problem with celebrities
+Find users who have never commented on a photo*/
+SELECT username,comment_text
+FROM users
+LEFT JOIN comments ON users.id = comments.user_id
+GROUP BY users.id
+HAVING comment_text IS NULL;
+
+SELECT COUNT(*) FROM
+(SELECT username,comment_text
+	FROM users
+	LEFT JOIN comments ON users.id = comments.user_id
+	GROUP BY users.id
+	HAVING comment_text IS NULL) AS total_number_of_users_without_comments;
+
+/*Mega Challenges
+Are we overrun with bots and celebrity accounts?
+Find the percentage of our users who have either never commented on a photo or have commented on every photo*/
+
+SELECT tableA.total_A AS 'Number Of Users who never commented',
+		(tableA.total_A/(SELECT COUNT(*) FROM users))*100 AS '%',
+		tableB.total_B AS 'Number of Users who likes every photos',
+		(tableB.total_B/(SELECT COUNT(*) FROM users))*100 AS '%'
+FROM
+	(
+		SELECT COUNT(*) AS total_A FROM
+			(SELECT username,comment_text
+				FROM users
+				LEFT JOIN comments ON users.id = comments.user_id
+				GROUP BY users.id
+				HAVING comment_text IS NULL) AS total_number_of_users_without_comments
+	) AS tableA
+	JOIN
+	(
+		SELECT COUNT(*) AS total_B FROM
+			(SELECT users.id,username, COUNT(users.id) As total_likes_by_user
+				FROM users
+				JOIN likes ON users.id = likes.user_id
+				GROUP BY users.id
+				HAVING total_likes_by_user = (SELECT COUNT(*) FROM photos)) AS total_number_users_likes_every_photos
+	)AS tableB;
+
+
+/*Find users who have ever commented on a photo*/
+SELECT username,comment_text
+FROM users
+LEFT JOIN comments ON users.id = comments.user_id
+GROUP BY users.id
+HAVING comment_text IS NOT NULL;
+
+
+SELECT COUNT(*) FROM
+(SELECT username,comment_text
+	FROM users
+	LEFT JOIN comments ON users.id = comments.user_id
+	GROUP BY users.id
+	HAVING comment_text IS NOT NULL) AS total_number_users_with_comments;
+
+
+/*Are we overrun with bots and celebrity accounts?
+Find the percentage of our users who have either never commented on a photo or have commented on photos before*/
+
+SELECT tableA.total_A AS 'Number Of Users who never commented',
+		(tableA.total_A/(SELECT COUNT(*) FROM users))*100 AS '%',
+		tableB.total_B AS 'Number of Users who commented on photos',
+		(tableB.total_B/(SELECT COUNT(*) FROM users))*100 AS '%'
+FROM
+	(
+		SELECT COUNT(*) AS total_A FROM
+			(SELECT username,comment_text
+				FROM users
+				LEFT JOIN comments ON users.id = comments.user_id
+				GROUP BY users.id
+				HAVING comment_text IS NULL) AS total_number_of_users_without_comments
+	) AS tableA
+	JOIN
+	(
+		SELECT COUNT(*) AS total_B FROM
+			(SELECT username,comment_text
+				FROM users
+				LEFT JOIN comments ON users.id = comments.user_id
+				GROUP BY users.id
+				HAVING comment_text IS NOT NULL) AS total_number_users_with_comments
+	)AS tableB
