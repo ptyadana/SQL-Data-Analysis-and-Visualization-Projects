cb79987aac9b70471e2c980817002b046b83ef61
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2021-01-19 14:37:48 +0800

    partition Data and query Partitioned Table (location-temp dataset)

---
+++ Advanced SQL for Data Science - Time Series/02.01.Modeling Time Series Data - Partitioning Data.sql
@@ -0,0 +1,81 @@
+/*********** Partitioning Data ****************/
+
+
+--------  Create new table with associated Partion Column (event hour: 0 - 24 hours) --------
+
+/* Create a table of location and temperature measurements */
+CREATE TABLE time_series.location_temp_p(
+	event_time TIMESTAMP NOT NULL,
+	event_hour INT,
+	temp_celcius INT,
+	location_id CHARACTER VARYING COLLATE pg_catalog."default"
+)PARTITION BY RANGE(event_hour);
+
+
+
+/* Create 12 partition nodes for that above table */
+-- range start value is inclusive, end value is exclusive
+CREATE TABLE time_series.location_temp_p1 PARTITION OF time_series.location_temp_p
+	FOR VALUES FROM (0) TO (2);
+
+-- together we will create index event_time for each partition node table
+CREATE INDEX idx_loc_temp_p1 ON time_series.location_temp_p1(event_time);
+
+
+
+/* continue creating another 11 nodes */
+CREATE TABLE time_series.location_temp_p2 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (2) TO (4);
+CREATE INDEX idx_loc_temp_p2 ON time_series.location_temp_p2(event_time);
+
+
+CREATE TABLE time_series.location_temp_p3 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (4) TO (6);
+CREATE INDEX idx_loc_temp_p3 ON time_series.location_temp_p3(event_time);
+
+
+CREATE TABLE time_series.location_temp_p4 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (6) TO (8);
+CREATE INDEX idx_loc_temp_p4 ON time_series.location_temp_p4(event_time);
+
+
+CREATE TABLE time_series.location_temp_p5 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (8) TO (10);
+CREATE INDEX idx_loc_temp_p5 ON time_series.location_temp_p5(event_time);
+
+
+CREATE TABLE time_series.location_temp_p6 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (10) TO (12);
+CREATE INDEX idx_loc_temp_p6 ON time_series.location_temp_p5(event_time);
+
+
+CREATE TABLE time_series.location_temp_p7 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (12) TO (14);
+CREATE INDEX idx_loc_temp_p7 ON time_series.location_temp_p7(event_time);
+
+
+CREATE TABLE time_series.location_temp_p8 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (14) TO (16);
+CREATE INDEX idx_loc_temp_p8 ON time_series.location_temp_p8(event_time);
+
+
+CREATE TABLE time_series.location_temp_p9 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (16) TO (18);
+CREATE INDEX idx_loc_temp_9 ON time_series.location_temp_p9(event_time);
+
+
+CREATE TABLE time_series.location_temp_p10 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (18) TO (20);
+CREATE INDEX idx_loc_temp_p10 ON time_series.location_temp_p10(event_time);
+
+
+CREATE TABLE time_series.location_temp_p11 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (20) TO (22);
+CREATE INDEX idx_loc_temp_p11 ON time_series.location_temp_p11(event_time);
+
+
+CREATE TABLE time_series.location_temp_p12 PARTITION OF time_series.location_temp_p
+    FOR VALUES FROM (22) TO (24);
+CREATE INDEX idx_loc_temp_p12 ON time_series.location_temp_p12(event_time);
+
+
---
+++ Advanced SQL for Data Science - Time Series/02.02.Modeling Time Series Data - Querying Partitioned Table.sql
@@ -0,0 +1,38 @@
+/*********** Querying Partitioned Table Data ****************/
+
+/* 1) get the average temp of march 5, 2019*/
+
+-- non partition version on original table: cost 34,090.84
+EXPLAIN SELECT location_id, AVG(temp_celcius)
+FROM time_series.location_temp
+WHERE event_time BETWEEN '2019-03-05' AND '2019-03-06'
+GROUP BY location_id;
+
+
+-- 2) partitioned version on new table: cost 166.23
+EXPLAIN SELECT location_id, AVG(temp_celcius)
+FROM time_series.location_temp_p
+WHERE event_time BETWEEN '2019-03-05' AND '2019-03-06'
+GROUP BY location_id;
+
+-- 3) partitioned version on new table using event hour: cost 78.52
+EXPLAIN SELECT location_id, AVG(temp_celcius)
+FROM time_series.location_temp_p
+WHERE event_hour BETWEEN 0 AND 4
+GROUP BY location_id;
+
+/*
+In summary the closer WHERE clause is to our partition, the more benefit we can get from Partitioning our data.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
